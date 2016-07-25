7656d3dfab2f35279c1d35f6ce8c0791799fb4f2
lrdwhyt <lrdwhyt@gmail.com>
2016-07-25 21:17:43 +0900

    code style changed by AS

---
+++ core/src/com/lrdwhyt/dispace/TouchHandler.java
@@ -0,0 +1,61 @@
+package com.lrdwhyt.dispace;
+
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.input.GestureDetector.GestureListener;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+
+public class TouchHandler implements GestureListener {
+
+  private Camera camera;
+
+  public TouchHandler(Camera camera) {
+    this.camera = camera;
+  }
+
+  @Override
+  public boolean touchDown(float x, float y, int pointer, int button) {
+    return false;
+  }
+
+  @Override
+  public boolean tap(float x, float y, int count, int button) {
+    System.out.println(camera.unproject(new Vector3(x, y, 0)));
+    return false;
+  }
+
+  @Override
+  public boolean longPress(float x, float y) {
+    return false;
+  }
+
+  @Override
+  public boolean fling(float velocityX, float velocityY, int button) {
+    return false;
+  }
+
+  @Override
+  public boolean pan(float x, float y, float deltaX, float deltaY) {
+    return false;
+  }
+
+  @Override
+  public boolean panStop(float x, float y, int pointer, int button) {
+    return false;
+  }
+
+  @Override
+  public boolean zoom(float initialDistance, float distance) {
+    return false;
+  }
+
+  @Override
+  public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+    return false;
+  }
+
+  @Override
+  public void pinchStop() {
+
+  }
+}
