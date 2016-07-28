c751a998010c38b7ef52274a123b93d5b2d4703d
lrdwhyt <lrdwhyt@gmail.com>
2016-07-28 22:09:59 +0900

    add framework for player and NPCs

---
+++ core/src/com/lrdwhyt/dispace/Character.java
@@ -0,0 +1,9 @@
+package com.lrdwhyt.dispace;
+
+public interface Character {
+
+  String getName();
+  Coords getPosition();
+  void setPosition(Coords position);
+
+}
---
+++ core/src/com/lrdwhyt/dispace/PlayerCharacter.java
@@ -0,0 +1,23 @@
+package com.lrdwhyt.dispace;
+
+public class PlayerCharacter implements Character {
+
+  private String name;
+  private Coords position;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Coords getPosition() {
+    return position;
+  }
+
+  public void setPosition(Coords position) {
+    this.position = position;
+  }
+}
