200f37cc72b317c4ca41718f2bbc513851c156de
lrdwhyt <lrdwhyt@gmail.com>
2016-07-28 22:00:20 +0900

    add framework for procedural map generation

--- core/src/com/lrdwhyt/dispace/World.java
+++ core/src/com/lrdwhyt/dispace/World.java
@@ -2,6 +2,8 @@ package com.lrdwhyt.dispace;
 
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
+import java.util.Random;
+
 public class World extends Map {
 
   private SpaceSet spaceSet;
@@ -12,13 +14,35 @@ public class World extends Map {
   }
 
   public void generate(int seed) {
+    Random rand = new Random();
     for (int x = 0; x < getWidth(); x++) {
       for (int y = 0; y < getHeight(); y++) {
-        int type = (int) (Math.random() * spaceSet.getLength());
+        int type = rand.nextInt(spaceSet.getLength());
         setAt(x, y, type);
       }
     }
   }
+
+  //TODO: Implement
+  public void generateTectonicMap() {
+    Map tectonicFramework = new Map(getWidth(), getHeight());
+    Random rand = new Random();
+    for (int x = 0; x < getWidth(); x++) {
+      for (int y = 0; y < getHeight(); y++) {
+        if (rand.nextInt(10) == 0) {
+          tectonicFramework.setAt(x, y, 1);
+        }
+      }
+    }
+    //TODO: Generate Voronoi diagram using tectonicFramework
+  }
+
+  //TODO: Implement
+  public void generateHeightMap(Map tectonicMap) {
+    //TODO: Generate random values for the boundaries given by the tectonic map
+    //TODO: Smooth out the heights
+  }
+
 
   public TextureRegion getSpace(int type) {
     return spaceSet.getSpaceType(type);
